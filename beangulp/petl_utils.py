772b0f091fc3a1d30622a88977e22c32ee4d94ad
blais <blais@furius.ca>
2021-11-21 22:32:47 -0500

    Add new utilities used to implement importers without mixins.

---
+++ beangulp/petl_utils.py
@@ -0,0 +1,63 @@
+"""Utilities using petl.
+"""
+
+import datetime
+import re
+
+import petl
+petl.config.look_style = 'minimal'
+petl.config.failonerror = True
+
+from beancount.core import data
+from beancount.core import amount
+from beancount.core import flags
+
+
+def table_to_directives(table: petl.Table, currency: str = 'USD') -> data.Entries:
+    """Convert a petl table to Beancount directives."""
+
+    # Ensure the table is sorted in order to produce the final balance.
+    assert table.issorted('date')
+
+    columns = table.fieldnames()
+    metas = []
+    for column in columns:
+        match = re.match("meta:(.*)", column)
+        if match:
+            metas.append((column, match.group(1)))
+
+    # Create transactions.
+    entries = []
+    for index, rec in enumerate(table.records()):
+        meta = data.new_metadata(f"<{__file__}>".format, index)
+        units = amount.Amount(rec.amount, currency)
+        tags, links = set(), set()
+        txn = data.Transaction(meta, rec.date, flags.FLAG_OKAY,
+                               getattr(rec, 'payee', None),
+                               getattr(rec, 'narration', ""),
+                               tags, links, [
+            data.Posting(rec.account, units, None, None, None, None)
+        ])
+
+        link = getattr(rec, 'link', None)
+        if link:
+            links.add(link)
+        tag = getattr(rec, 'tag', None)
+        if tag:
+            tags.add(link)
+
+        for column, key in metas:
+            value = getattr(rec, column, None)
+            if value:
+                meta[key] = value
+        entries.append(txn)
+
+    if 'balance' in columns:
+        # Insert a balance with the final value.
+        meta = data.new_metadata(f"<{__file__}>", index + 1)
+        balance_date = rec.date + datetime.timedelta(days=1)
+        entries.append(data.Balance(
+            meta, balance_date, rec.account, amount.Amount(rec.balance, currency),
+            None, None))
+
+    return entries
--- beangulp/utils.py
+++ beangulp/utils.py
@@ -1,11 +1,16 @@
+from decimal import Decimal
+from os import path
+from typing import Iterator, Sequence, Union, Set, Optional, Dict
+import datetime
+import decimal
 import hashlib
 import os
-import datetime
-
-from os import path
-from typing import Iterator, Sequence
+import re
 
 import click
+import dateutil.parser
+
+from beangulp import mimetypes
 
 
 def getmdate(filepath: str) -> datetime.date:
@@ -51,3 +56,78 @@ def sha1sum(filepath: str) -> str:
     """Compute hash of the file at filepath."""
     with open(filepath, 'rb') as fd:
         return hashlib.sha1(fd.read()).hexdigest()
+
+
+def is_mimetype(filepath: str, check_mimetypes: Union[str, Set[str]],
+                regexp: Optional[bool] = False) -> bool:
+    """Check if a file is of one of many mimetypes."""
+    if isinstance(check_mimetypes, str):
+        check_mimetypes = set((check_mimetypes,))
+    mtype, _ = mimetypes.guess_type(filepath)
+    return (any(re.match(r, mtype) for r in check_mimetypes)
+            if not regexp else
+            (mtype in check_mimetypes))
+
+
+def search_file_regexp(filepath: str, *regexps: str,
+                       nbytes: Optional[int] = None,
+                       encoding: Optional[str] = None) -> bool:
+    """Check if the header of the file matches the given regexp."""
+    with open(filepath, encoding=encoding) as infile:
+        # Note: Don't convert just to match on the contents.
+        contents = infile.read(nbytes)
+        return any(re.search(regexp, contents) for regexp in regexps)
+
+
+def parse_date(string: str) -> datetime.date:
+    """Parse any date format into a date string."""
+    return dateutil.parser.parse(string).date()
+
+
+def parse_amount(string: str)-> decimal.Decimal:
+    """Convert an amount with parens and dollar sign to Decimal."""
+    if string is None:
+        return Decimal(0)
+    string = string.strip()
+    if not string:
+        return Decimal(0)
+    match = re.match(r"\((.*)\)", string)
+    if match:
+        string = match.group(1)
+        sign = -1
+    else:
+        sign = 1
+    cstring = string.strip(' $').replace(',', '')
+    try:
+        return Decimal(cstring) * sign
+    except decimal.InvalidOperation as exc:
+        raise decimal.InvalidOperation(f"Invalid conversion of {cstring!r}") from exc
+
+
+def validate_accounts(required_accounts: Dict[str, str], provided_accounts: Dict[str, str]):
+    """Check a dict of provided account names against a specification of required ones.
+
+    Args:
+      required_accounts: A dict of declarations of required values.
+      provided_accounts: A config dict of actual values on an importer.
+    Raises:
+      ValueError: If the configuration is invalid.
+    """
+    # Note: As an extension, we could provide the existing ledger and try to
+    # validate the non-template names against the list of accounts declared in
+    # it.
+
+    provided_keys = set(provided_accounts)
+    required_keys = set(required_accounts)
+
+    for key in (required_keys - provided_keys):
+        raise ValueError(f"Missing value from user configuration: '{key}'; "
+                         "against {required_keys}")
+
+    for key in (provided_keys - required_keys):
+        raise ValueError(f"Unknown value in user configuration: '{key}'; "
+                         f"against {required_keys}")
+
+    for account in provided_accounts.values():
+        if not isinstance(account, str):
+            raise ValueError(f"Invalid value for account or currency: '{account}'")
